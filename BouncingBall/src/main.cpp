1527ef2c03c44c0b32b830d9f03d563fd49a3329
maketvee <martin.oehler@mail.de>
2021-05-07 18:00:34 +0200

    refactoring

--- BouncingBall/src/main.cpp
+++ BouncingBall/src/main.cpp
@@ -9,36 +9,33 @@
 
 npNeoPixel pixels = npNeoPixel(MAXPIXELS, NeoPin, NEO_GRB + NEO_KHZ800, 8.0);
 //-------------------------------------------------------------------------------------
-unsigned int Delay = 100;
-
-float Motion_Theshold = 10.0;
 
 // define 6 virtual strips 
-// reversed for bouncing ball effect QD->fix
-npVirtualNeo vNeo1(&pixels,  19, 0);
+// reversed for bouncing ball effect (counting from bottom to top)
+npVirtualNeo vNeo1(&pixels, 19, 0);
 npVirtualNeo vNeo2(&pixels, 20, 39);
 npVirtualNeo vNeo3(&pixels, 59, 40);
-npVirtualNeo vNeo4(&pixels,  60, 79);
+npVirtualNeo vNeo4(&pixels, 60, 79);
 npVirtualNeo vNeo5(&pixels, 99, 80);
 npVirtualNeo vNeo6(&pixels, 100, 119);
 
 //array of bouncing balls for easier access, different initial delay
-npBouncingBall bballs[NSTRIPES] = {npBouncingBall(50,vNeo1),npBouncingBall(60,vNeo2),npBouncingBall(40,vNeo3),npBouncingBall(20,vNeo4),npBouncingBall(40,vNeo5),npBouncingBall(70,vNeo6)};
+npBouncingBall bballs[NSTRIPES] = {npBouncingBall(50,vNeo1),npBouncingBall(60,vNeo2),
+                                    npBouncingBall(40,vNeo3),npBouncingBall(20,vNeo4),
+                                    npBouncingBall(40,vNeo5),npBouncingBall(70,vNeo6)};
 
-byte counter=1;
-byte *c;
 
-float diffx,diffy,diffz,diff;
 sensors_event_t accel_old;
-
 Adafruit_LSM6DSO32 dso32;
-
-byte * Wheel(byte WheelPos);
+float diff[3] = {0,0,0};
+float diffsum;
+byte *c;
+float Motion_Theshold = 10.0;
 
 //-------------------------------------------------------------------------------------
 void setup ()
 {
-    
+  // setup LSM6DS032 sensor
   if (!dso32.begin_I2C()) {
     while (1) {
       delay(10);
@@ -47,10 +44,6 @@ void setup ()
   dso32.setAccelRange(LSM6DSO32_ACCEL_RANGE_8_G);
   dso32.setAccelDataRate(LSM6DS_RATE_12_5_HZ);
 
-  diffx=0;
-  diffz=0;
-  diffy=0;
-  Serial.begin (115200); // Serial output with 115200 bps
   pixels.begin();
   pixels.clear();
   pixels.npShow();
@@ -63,27 +56,29 @@ void loop ()
   sensors_event_t temp;
   dso32.getEvent(&accel, &gyro, &temp);
 
-  diffx=abs(accel.acceleration.x - accel_old.acceleration.x);
-  diffy=abs(accel.acceleration.y - accel_old.acceleration.y);
-  diffz=abs(accel.acceleration.z - accel_old.acceleration.z);
+  //calculating the diff to previous value for x,y and z axis
+  diff[0]=abs(accel.acceleration.x - accel_old.acceleration.x);
+  diff[1]=abs(accel.acceleration.y - accel_old.acceleration.y);
+  diff[2]=abs(accel.acceleration.z - accel_old.acceleration.z);
   
   accel_old=accel;
-  diff=diffx+diffy+diffz;
+  //react to all axis
+  diffsum=diff[0]+diff[1]+diff[2];
   
   for(int i=0;i<NSTRIPES;i++) bballs[i].update();
 
-  if (bballs[4].hasFinished() && (diff>Motion_Theshold))
+  if (bballs[3].hasFinished() && (diffsum>Motion_Theshold))
   {
-    c=Wheel(random(0,255));
+    //c=Wheel(random(0,255));
 
     for(int i=0;i<NSTRIPES;i++) 
     {
-      //fixed collor for the balls
+      //uncomment following line for fixed collor for the balls
+      //and the upper line c=Wheel... 
       //bballs[i].changeColor(*c, *(c+1), *(c+2));
       bballs[i].restart();
     }
   }
-  
 }
 
 byte * Wheel(byte WheelPos) {
